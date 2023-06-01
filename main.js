fff566787d1a9803aef78840ec339af3376f7cb3
Pellyyy <zabookinkajou@protonmail.com>
2023-06-01 12:44:43 +0200

    add vertices randomized movement

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const hoverColor = {
     g: 187 / 255,
     b: 153 / 255,
 }
+// random values to move the vertices
+let randomValues = []
 // dat.gui
 const gui = new dat.GUI()
 const world = {
@@ -41,6 +43,10 @@ function transformVertices() {
         vertices[i] = x + (Math.random() - 0.5) * 2
         vertices[i + 1] = y + (Math.random() - 0.5) * 2
         vertices[i + 2] = (z + Math.random() - 0.5) * 7.5
+
+        for (let j = 0; j < 3; ++j) {
+            randomValues.push(Math.random() - 0.5)
+        }
     }
 }
 
@@ -54,14 +60,28 @@ function generatePlaneGeometry() {
     )
     transformVertices()
 }
+
+let time = 0
+
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
     raycaster.setFromCamera(mouse, camera)
+    time += 0.001
+
+    const { array } = planeMesh.geometry.attributes.position
+
+    for (let i = 0; i < array.length; i += 3) {
+        array[i] = array[i] + Math.cos(time + randomValues[i]) * 0.005
+        array[i + 1] =
+            array[i + 1] + Math.sin(time + randomValues[i + 1]) * 0.005
+        if (i === 0) {
+        }
+    }
     const intersections = raycaster.intersectObject(planeMesh)
     const { color } = planeMesh.geometry.attributes
     color.needsUpdate = true
-
+    planeMesh.geometry.attributes.position.needsUpdate = true
     if (intersections.length > 0) {
         color.setXYZ(
             intersections[0].face.a,
@@ -138,7 +158,9 @@ const planeMaterial = new THREE.MeshPhongMaterial({
     flatShading: true,
     vertexColors: true,
 })
+
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+console.log(planeMesh.geometry.attributes)
 const light = new THREE.DirectionalLight(0xfdfbd3, 1)
 const backLight = new THREE.DirectionalLight(0xffffff, 1)
 
@@ -154,11 +176,11 @@ planeMesh.geometry.setAttribute(
 
 transformVertices()
 
-light.position.set(0, 0.75, 1)
+light.position.set(0, 1, 1)
 backLight.position.set(0, 1, -1)
 
 new OrbitControls(camera, renderer.domElement)
-camera.position.z = 100 // move camera towards us (z-axis)
+camera.position.z = 75 // move camera towards us (z-axis)
 
 scene.add(planeMesh)
 scene.add(light)
